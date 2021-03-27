f587f5f924402803886f39e7f0594a7a01983c5d
osmarks <osmarks@protonmail.com>
2021-03-27 11:56:05 +0000

    Improve parser It now does IF, RETURN, SUBROUTINE. No array value assignment yet, or USERINPUT/OUTPUT (these SHOULD NOT be keywords, whyyyy AQA)

--- parse.py
+++ parse.py
@@ -6,13 +6,43 @@ from parsita.util import constant
 
 def compose(f, g): return lambda x: f(g(x))
 
+op_map = {
+    "+": "add",
+    "-": "sub",
+    "*": "mul",
+    "%": "mod"
+}
+
 def map_expr(x):
     start, end = x
-    if end == "": return start
-    return Op([start, end[1]], end[0])
+    if end == None: return start
+    opname = end[0]
+    if opname == "index":
+        return Index(start, end[1])
+    elif opname == "call":
+        return Call(start, end[1:])
+    else:
+        return Op([start, end[1]], op_map.get(end[0], end[0]))
 
 def map_unop_expr(x):
     return Op(x[1], x[0])
+
+def map_if(x):
+    def map_ifc(x):
+        if x == None:
+            return [], []
+        elif x[0] == "ELSE":
+            return [], x[1]
+        else:
+            elif_cases, else_cases = map_ifc(x[4])
+            return [(x[1], x[3])] + elif_cases, else_cases
+    elif_cases, else_case = map_ifc(x[4])
+    return If([
+        (x[1], x[3]),
+    ] + elif_cases, else_case)
+
+def map_subroutine(x):
+    return Function(x[3], x[5], x[1])
 
 def aliases(name, aliases):
     p = lit(name)
@@ -21,59 +51,74 @@ def aliases(name, aliases):
     return p
 
 class PseudocodeParser(TextParsers):
-    ε = lit("")
+    ε = lit("") > (lambda _: None)
     IntLit = reg("\-?[0-9]+") > compose(IntLit, int)
     StrLit = "'" >> reg("[^']*") << "'" > StrLit # TODO escapes (not in "spec" but could be needed)
     FloatLit = reg("\-?[0-9]+\.[0-9]+") > compose(FloatLit, float)
     ArrayLit = "[" >> repsep(Expr, ",") << "]" > ArrayLit
-    Identifier = reg("[a-zA-Z_]+[a-zA-Z_0-9]*") > Var
+    Identifier = reg("[a-zA-Z_]+[a-zA-Z_0-9]*")
+    VarUse = Identifier > SymbLit
     BracketedExpr = "(" >> Expr << ")"
     UnaryOperator = lit("NOT")
-    Start = FloatLit | StrLit | IntLit | ArrayLit | BracketedExpr | (UnaryOperator & Expr > map_unop_expr) | Identifier
+    Start = FloatLit | StrLit | IntLit | ArrayLit | BracketedExpr | (UnaryOperator & Expr > map_unop_expr) | VarUse
     # avoid left recursion problems by not doing left recursion
     # AQA pseudocode does not appear to have a notion of "operator precedence", simplifying parsing logic nicely
-    Index = "[" >> Expr << "]" > (lambda x: ["[]", x])
+    Index = "[" >> Expr << "]" > (lambda x: ["index", x])
+    Call = "(" >> repsep(Expr, ",") << ")" > (lambda x: ["call"] + x)
     BinaryOperator = aliases("≤", ["<="]) | aliases("≠", ["!="]) | aliases("≥", [">="]) | lit("DIV") | lit("MOD") | lit("AND") | lit("OR") | reg("[+/*\-=<>]")
-    End = ((BinaryOperator & Expr) | Index) | ε
+    End = ((BinaryOperator & Expr) | Index | Call) | ε
     Expr = (Start & End) > map_expr
 
     AssignmentOp = lit("<-") | lit("←")
-    Assignment = Identifier & AssignmentOp & Expr
+    Assignment = (Identifier > Var) & AssignmentOp & Expr > (lambda l: Assignment(l[0], l[2]))
     ConstAssignment = lit("constant") & Assignment
-    RepeatUntil = lit("REPEAT") & Program & lit("UNTIL") & Expr
-    While = lit("WHILE") & Expr & Program & lit("ENDWHILE")
-    For = lit("FOR") & Identifier & AssignmentOp & Expr & lit("TO") & Expr & Program & lit("ENDFOR")
-    IfBody = (lit("ELSE IF") & Expr & lit("THEN") & Program & IfBody) | (lit("ELSE") & Program) | ε
-    If = lit("IF") & Expr & lit("THEN") & Program & IfBody & lit("ENDIF")
-    Statement = For | If | While | RepeatUntil | Assignment | ConstAssignment
+    RepeatUntil = lit("REPEAT") & Program & lit("UNTIL") & Expr > (lambda l: Repeat(l[1], l[3]))
+    While = lit("WHILE") & Expr & Program & lit("ENDWHILE") > (lambda l: While(l[2], l[1]))
+    For = lit("FOR") & Identifier & AssignmentOp & Expr & lit("TO") & Expr & Program & lit("ENDFOR") > (lambda l: For(l[1], l[3], l[5], l[6]))
+    IfContinuation = (lit("ELSE IF") & Expr & lit("THEN") & Program & IfContinuation) | (lit("ELSE") & Program) | ε
+    If = lit("IF") & Expr & lit("THEN") & Program & IfContinuation & lit("ENDIF") > map_if
+    Subroutine = lit("SUBROUTINE") & Identifier & lit("(") & repsep(Identifier, lit(",")) & lit(")") & Program & lit("ENDSUBROUTINE") > map_subroutine
+    Return = lit("RETURN") >> Expr > Return
+    Statement = Subroutine | Return | For | If | While | RepeatUntil | Assignment | ConstAssignment
     Program = rep(Statement)
 
-parse = PseudocodeParser.Expr.parse
-x = parse("1+2+3 != 6 AND NOT 4 AND x + y + [1,2, 3][5]")
-if isinstance(x, Failure):
-    print(x.message)
-else:
-    print(x.value)
-x = PseudocodeParser.Program.parse("""
-FOR I <- 1 TO 3
-    WHILE 4
-        IF i < 1248124 THEN
-            REPEAT a ← b UNTIL b
-        ENDIF
-        IF i < 4 THEN
-            y ← 4
-        ELSE
-            y ← 5
-        ENDIF
-        IF i < 9 THEN
-            y ← 4
-        ELSE IF i < 10 THEN
-            y ← 5
-        ENDIF
-    ENDWHILE
-ENDFOR
-""".strip())
-if isinstance(x, Failure):
-    print(x.message)
-else:
-    print(x.value)
+parse = PseudocodeParser.Program.parse
+if __name__ == "__main__":
+    x = parse("1+2+3 != 6 AND NOT 4 AND x + y + [1,2, 3][5]")
+    if isinstance(x, Failure):
+        print(x.message)
+    else:
+        print(x.value)
+    x = PseudocodeParser.Program.parse("""
+    SUBROUTINE thing(a, b, c)
+        x ← 4
+        y ← apify(a,b,c)
+        RETURN LEN(a)
+    ENDSUBROUTINE
+    x ← 4
+    FOR I <- 1 TO 3
+        WHILE 4
+            REPEAT bees ← 4 UNTIL 5
+            IF i < 1248124 THEN
+                REPEAT a ← b UNTIL b
+            ENDIF
+            IF i < 4 THEN
+                y ← 4
+            ELSE
+                y ← 5
+            ENDIF
+            IF i < 9 THEN
+                y ← 4
+            ELSE IF i < 10 THEN
+                y ← 5
+            ELSE IF i < 11 THEN
+                y ← 6
+            ENDIF
+        ENDWHILE
+    ENDFOR
+    """.strip())
+    if isinstance(x, Failure):
+        print(x.message)
+    else:
+        print(x.value)
+        print("\n".join(map(lambda x: x.show(), x.value)))
